f4d5e483ff8da3fe62a276de8ebadb171d42b939
Jimmie Butler <jimmiebtlr@gmail.com>
2015-12-15 08:42:59 -0600

    Only export on server.

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'poetic:harvest',
-  version: '0.1.4',
+  version: '0.1.5-rc.1',
   summary: 'Packaged harvest plugin.',
   git: 'https://github.com/poetic/meteor-harvest.git',
   documentation: 'README.md'
@@ -8,9 +8,9 @@ Package.describe({
 
 Package.onUse(function(api) {
   api.versionsFrom('1.2.0.2');
-  api.addFiles('harvest.js');
+  api.addFiles('harvest.js', 'server');
 
-  api.export( 'Harvest' );
+  api.export( 'Harvest','server' );
 });
 
 Npm.depends({harvest: '0.1.4'});
